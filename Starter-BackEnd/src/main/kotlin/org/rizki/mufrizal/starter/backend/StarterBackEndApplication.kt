05900e422155d95d5ecc944d8a51658be531ac87
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-08-20 17:38:04 +0700

    initial project backend

---
+++ Starter-BackEnd/src/main/kotlin/org/rizki/mufrizal/starter/backend/StarterBackEndApplication.kt
@@ -0,0 +1,11 @@
+package org.rizki.mufrizal.starter.backend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class StarterBackEndApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(StarterBackEndApplication::class.java, *args)
+}
